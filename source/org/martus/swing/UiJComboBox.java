e08d3e63f6d1a684264e0071cd5b592c5f286bf6
charles <charles@localhost>
2004-09-29 17:52:51 +0000

    combobox support for right to left languages.

---
+++ source/org/martus/swing/UiJComboBox.java
@@ -0,0 +1,62 @@
+/*
+
+The Martus(tm) free, social justice documentation and
+monitoring software. Copyright (C) 2001-2004, Beneficent
+Technology, Inc. (Benetech).
+
+Martus is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later
+version with the additions and exceptions described in the
+accompanying Martus license file entitled "license.txt".
+
+It is distributed WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, including warranties of fitness of purpose or
+merchantability.  See the accompanying Martus License and
+GPL license for more details on the required license terms
+for this software.
+
+You should have received a copy of the GNU General Public
+License along with this program; if not, write to the Free
+Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.
+
+*/
+package org.martus.swing;
+
+import java.awt.Component;
+import javax.swing.DefaultListCellRenderer;
+import javax.swing.JComboBox;
+import javax.swing.JList;
+
+
+public class UiJComboBox extends JComboBox
+{
+	public UiJComboBox()
+	{
+		setComponentOrienation();
+	}
+	
+	public UiJComboBox(Object[] items)
+	{
+		super(items);
+		setComponentOrienation();
+	}
+
+	private class JComboListCellRenderer extends DefaultListCellRenderer
+	{
+		public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus)
+		{
+			setHorizontalAlignment(UiLanguageDirection.getHorizontalAlignment());
+			return super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+		}
+	}
+	
+	private void setComponentOrienation()
+	{
+		setComponentOrientation(UiLanguageDirection.getComponentOrientation());
+		setRenderer(new JComboListCellRenderer());
+	}
+	
+}
